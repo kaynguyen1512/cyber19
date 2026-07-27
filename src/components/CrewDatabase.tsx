a40c6cc90837bad4f3adc920473bccc4da19b551
ledai00777 <ledai00777@gmail.com>
2026-07-27 18:45:45 +0700

    Updated CrewDatabase.tsx

--- src/components/CrewDatabase.tsx
+++ src/components/CrewDatabase.tsx
@@ -437,7 +437,7 @@ export default function CrewDatabase() {
       <CrewFXStyles />
 
       {/* Scroll runway */}
- <div style={{ height: '480hv', position: 'relative' }}>
+ <div style={{ height: '480vh', position: 'relative' }}>
         {/* Pinned viewport */}
         <div
           style={{
